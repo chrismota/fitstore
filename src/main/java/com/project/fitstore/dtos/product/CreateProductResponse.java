fa9745ad581a3fceb90d42661604e696f8fdd4f3
Christian Mota <christiancorreiamota@gmail.com>
2025-03-07 15:56:24 -0300

    adding imagePath field on product endpoint responses

--- src/main/java/com/project/fitstore/dtos/product/CreateProductResponse.java
+++ src/main/java/com/project/fitstore/dtos/product/CreateProductResponse.java
@@ -7,9 +7,9 @@ import com.project.fitstore.domain.product.SubCategory;
 import java.math.BigDecimal;
 
 public record CreateProductResponse(Long id, String name, String description, String brand, Category category,
-                                    SubCategory subCategory, BigDecimal price) {
+                                    SubCategory subCategory, BigDecimal price, String imagePath) {
     public static CreateProductResponse from(Product product) {
         return new CreateProductResponse(product.getId(), product.getName(), product.getDescription(),
-                product.getBrand(), product.getCategory(), product.getSubCategory(), product.getPrice());
+                product.getBrand(), product.getCategory(), product.getSubCategory(), product.getPrice(), product.getImagePath());
     }
 }
--- src/main/java/com/project/fitstore/dtos/product/GetProductResponse.java
+++ src/main/java/com/project/fitstore/dtos/product/GetProductResponse.java
@@ -7,9 +7,9 @@ import com.project.fitstore.domain.product.SubCategory;
 import java.math.BigDecimal;
 
 public record GetProductResponse(Long id, String name, String description, String brand, Category category,
-                                 SubCategory subCategory, BigDecimal price) {
+                                 SubCategory subCategory, BigDecimal price, String imagePath) {
     public static GetProductResponse from(Product product) {
         return new GetProductResponse(product.getId(), product.getName(), product.getDescription(), product.getBrand(),
-                product.getCategory(), product.getSubCategory(), product.getPrice());
+                product.getCategory(), product.getSubCategory(), product.getPrice(), product.getImagePath());
     }
 }
