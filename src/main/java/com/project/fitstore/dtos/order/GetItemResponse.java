81648fd1a8f9f05485a11e8ee2e9f6614f6b4e56
Christian Mota <christiancorreiamota@gmail.com>
2025-03-07 16:05:07 -0300

    adding imagePath field on get order endpoint response

--- src/main/java/com/project/fitstore/dtos/order/GetItemResponse.java
+++ src/main/java/com/project/fitstore/dtos/order/GetItemResponse.java
@@ -4,9 +4,9 @@ import com.project.fitstore.domain.OrderItem.OrderItem;
 
 import java.math.BigDecimal;
 
-public record GetItemResponse(Long id, String productName, Integer quantity, BigDecimal unityPrice) {
+public record GetItemResponse(Long id, String productName, Integer quantity, BigDecimal unityPrice, String imagePath) {
     public static GetItemResponse from(OrderItem orderItem) {
         return new GetItemResponse(orderItem.getProduct().getId(), orderItem.getProduct().getName(),
-                orderItem.getQuantity(), orderItem.getUnityPrice());
+                orderItem.getQuantity(), orderItem.getUnityPrice(), orderItem.getProduct().getImagePath());
     }
 }
