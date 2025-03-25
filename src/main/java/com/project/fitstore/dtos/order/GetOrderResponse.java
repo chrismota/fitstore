1e8666195e898373e92f01952b92073aa86cb58d
Christian Mota <christiancorreiamota@gmail.com>
2025-03-25 14:36:02 -0300

    feat: added created date field to order response dto

--- src/main/java/com/project/fitstore/dtos/order/GetOrderResponse.java
+++ src/main/java/com/project/fitstore/dtos/order/GetOrderResponse.java
@@ -4,12 +4,13 @@ import com.project.fitstore.domain.order.Order;
 import com.project.fitstore.domain.order.Status;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public record GetOrderResponse(UUID id, BigDecimal discount, BigDecimal totalValue, BigDecimal finalValue,
-                               Status status, List<GetItemResponse> items) {
+                               Status status, LocalDateTime createdAt, List<GetItemResponse> items) {
     public static GetOrderResponse from(Order order) {
-        return new GetOrderResponse(order.getId(), order.getDiscount(), order.getFullValue(), order.getValueAfterDiscount(), order.getStatus(), order.getItems().stream().map(GetItemResponse::from).toList());
+        return new GetOrderResponse(order.getId(), order.getDiscount(), order.getFullValue(), order.getValueAfterDiscount(), order.getStatus(), order.getCreatedAt(), order.getItems().stream().map(GetItemResponse::from).toList());
     }
 }
