6448e28d9c1ce95ebe2d2cc091009190e795bcf3
Christian Mota <christiancorreiamota@gmail.com>
2025-06-23 18:59:16 -0300

    style: correcting indentation in some files

--- src/main/java/com/project/fitstore/dtos/product/UpdateProductResponse.java
+++ src/main/java/com/project/fitstore/dtos/product/UpdateProductResponse.java
@@ -9,7 +9,8 @@ import java.math.BigDecimal;
 public record UpdateProductResponse(Long id, String name, String description, String brand, String sku,
                                     Category category, SubCategory subCategory, BigDecimal price, String imagePath) {
     public static UpdateProductResponse from(Product product) {
-        return new UpdateProductResponse(product.getId(), product.getName(), product.getDescription(), product.getBrand(), product.getSku(),
-                product.getCategory(), product.getSubCategory(), product.getPrice(), product.getImagePath());
+        return new UpdateProductResponse(product.getId(), product.getName(), product.getDescription(), product.getBrand(),
+                product.getSku(), product.getCategory(), product.getSubCategory(), product.getPrice(),
+                product.getImagePath());
     }
 }
