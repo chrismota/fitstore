36cc6064f3e40023c01c6152372b61acb5ab99d8
Christian Mota <christiancorreiamota@gmail.com>
2025-07-04 21:28:37 -0300

    feat: adding total field for items from order

--- src/main/java/com/project/fitstore/dtos/order/CreateItemResponse.java
+++ src/main/java/com/project/fitstore/dtos/order/CreateItemResponse.java
@@ -4,9 +4,14 @@ import com.project.fitstore.domain.OrderItem.OrderItem;
 
 import java.math.BigDecimal;
 
-public record CreateItemResponse(Long id, String productName, Integer quantity, BigDecimal unityPrice, String imagePath) {
+public record CreateItemResponse(
+        Long id, String productName,
+        BigDecimal unityPrice, Integer quantity,
+        BigDecimal total, String imagePath) {
     public static CreateItemResponse from(OrderItem orderItem) {
-        return new CreateItemResponse(orderItem.getProduct().getId(), orderItem.getProduct().getName(),
-                orderItem.getQuantity(), orderItem.getUnityPrice(), orderItem.getProduct().getImagePath());
+        return new CreateItemResponse(
+                orderItem.getProduct().getId(), orderItem.getProduct().getName(),
+                orderItem.getUnityPrice(), orderItem.getQuantity(),
+                orderItem.getTotal(), orderItem.getProduct().getImagePath());
     }
 }
--- src/main/java/com/project/fitstore/dtos/order/GetItemResponse.java
+++ src/main/java/com/project/fitstore/dtos/order/GetItemResponse.java
@@ -4,9 +4,14 @@ import com.project.fitstore.domain.OrderItem.OrderItem;
 
 import java.math.BigDecimal;
 
-public record GetItemResponse(Long id, String productName, Integer quantity, BigDecimal unityPrice, String imagePath) {
+public record GetItemResponse(
+        Long id, String productName,
+        BigDecimal unityPrice, Integer quantity,
+        BigDecimal total, String imagePath) {
     public static GetItemResponse from(OrderItem orderItem) {
-        return new GetItemResponse(orderItem.getProduct().getId(), orderItem.getProduct().getName(),
-                orderItem.getQuantity(), orderItem.getUnityPrice(), orderItem.getProduct().getImagePath());
+        return new GetItemResponse(
+                orderItem.getProduct().getId(), orderItem.getProduct().getName(),
+                orderItem.getUnityPrice(), orderItem.getQuantity(),
+                orderItem.getTotal(), orderItem.getProduct().getImagePath());
     }
 }
